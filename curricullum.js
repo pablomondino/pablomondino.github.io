0170aedcd5c76b4b1f6fe664a118bc5a8ec12775
Pablo Mondino De La Rossa <69263740+pablomondino@users.noreply.github.com>
2023-01-30 17:25:02 -0300

    commit inicial

---
+++ curricullum.js
@@ -0,0 +1,59 @@
+// BOTON CAMBIAR ESTILO
+
+
+document.getElementById("boton").onclick=() => {
+    console.log("capturar evento");
+    document.getElementById("demo").innerHTML="Si presionas el botón reset, la pagina vueve a su estado anterior."
+    document.getElementById("sobre-mi").style.background = "#0E5CBC ";
+   
+}
+
+
+// BOTON RESET
+
+
+document.getElementById("boton-reset").onclick=() => {
+    console.log("capturar evento se presiono boton reset");
+    document.getElementById("demo").innerHTML="          "
+    document.getElementById("sobre-mi").style.background = "#060000";
+   
+}
+
+
+// BOTON VER 
+
+
+document.getElementById("boton_ver").addEventListener("click", function () {
+
+   
+    let elemento1=document.createElement("a");
+    
+   
+    document.getElementById("agregar").appendChild(elemento1);
+
+    elemento1.href="imagenes/Curriculum_vitae_Pablo_Mondino_De_La_Rossa_enero2023.pdf";
+    elemento1.target="_blank";
+    elemento1.click();
+ 
+})
+
+
+
+
+// BOTON DESCARGAR
+
+
+document.getElementById("boton_descarga").addEventListener("click", function () {
+
+    
+    let elemento1=document.createElement("a");
+    
+    document.getElementById("agregar").appendChild(elemento1);
+
+    elemento1.href="imagenes/Curriculum_vitae_Pablo_Mondino_De_La_Rossa_enero2023.pdf";
+    elemento1.target="_blank";
+    elemento1.download= "Curriculum_vitae_Pablo_Mondino_De_La_Rossa_enero2023.pdf";
+    elemento1.click();
+  
+})
+
