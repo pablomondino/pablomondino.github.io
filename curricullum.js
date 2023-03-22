1e16012553b01177cf69322c61c6e2e62acaaabc
Pablo Mondino De La Rossa <69263740+pablomondino@users.noreply.github.com>
2023-03-22 16:40:29 -0300

    cambie las referencia de los archivos de js
    en donde linkea el curriculum

--- curricullum.js
+++ curricullum.js
@@ -31,7 +31,7 @@ document.getElementById("boton_ver").addEventListener("click", function () {
    
     document.getElementById("agregar").appendChild(elemento1);
 
-    elemento1.href="imagenes/Curriculum_vitae_Pablo_Mondino_De_La_Rossa_enero2023.pdf";
+    elemento1.href="imagenes/Curriculum_vitae_Pablo_Mondino_De_La_Rossa_febrero2023 (2)";
     elemento1.target="_blank";
     elemento1.click();
  
@@ -52,7 +52,7 @@ document.getElementById("boton_descarga").addEventListener("click", function () 
 
     elemento1.href="Curriculum_vitae_Pablo_Mondino_De_La_Rossa_febrero2023 (2)";
     elemento1.target="_blank";
-    elemento1.download= "Curriculum_vitae_Pablo_Mondino_De_La_Rossa_febrero2023 (2)";
+    elemento1.download= "imagenes/Curriculum_vitae_Pablo_Mondino_De_La_Rossa_febrero2023 (2)";
     elemento1.click();
   
 })
